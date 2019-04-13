2dcc4eee588950fb9bdeac7b686341d1bad13ce3
lizhongyue248 <lizhongyue248@163.com>
2019-04-13 11:16:51 +0800

    :art: Improving structure / format of the redis.

--- lesson-cloud-core/src/main/java/cn/edu/gzmu/config/RedisConfig.java
+++ lesson-cloud-core/src/main/java/cn/edu/gzmu/config/RedisConfig.java
@@ -5,16 +5,34 @@ package cn.edu.gzmu.config;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.spring.data.connection.RedissonConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+
 /**
  * @author echo
  * @version 1.0
  * @date 19-4-12 09:50
  */
-//@Configuration
+@Configuration
 public class RedisConfig {
 
-//    @Bean
-//    public RedissonClient redissonClient(){
-//        return Redisson.create();
-//    }
+    @Bean
+    public RedissonConnectionFactory redissonConnectionFactory(RedissonClient redisson) {
+        return new RedissonConnectionFactory(redisson);
+    }
+
+    @Bean(destroyMethod = "shutdown")
+    public RedissonClient redisson(@Value("classpath:/redisson.yaml") Resource configFile) throws IOException {
+        Config config = Config.fromYAML(configFile.getInputStream());
+        return Redisson.create(config);
+    }
+
 }
