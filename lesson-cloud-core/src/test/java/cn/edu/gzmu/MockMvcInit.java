47f4421c08a247354907bc0da34d8da47b2cbf27
lizhongyue248 <lizhongyue248@163.com>
2019-04-13 15:24:05 +0800

    :white_check_mark: Add controller test for one.

---
+++ lesson-cloud-core/src/test/java/cn/edu/gzmu/MockMvcInit.java
@@ -0,0 +1,88 @@
+package cn.edu.gzmu;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+
+/**
+ * @author echo
+ * @version 1.0
+ * @date 19-4-13 12:53
+ */
+public abstract class MockMvcInit {
+
+    @Autowired
+    private WebApplicationContext ctx;
+
+    protected MockMvc mockMvc;
+    protected ObjectMapper mapper = new ObjectMapper();
+
+    @Before
+    public void init(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        mapper.registerModule(new ParameterNamesModule())
+                .registerModule(new Jdk8Module())
+                .registerModule(new JavaTimeModule());
+    }
+
+    /**
+     * 获取一个资源
+     *
+     * @throws Exception 异常
+     */
+    public abstract void testGetResources() throws Exception;
+
+    /**
+     * 获取已经存在的资源
+     *
+     * @throws Exception 异常
+     */
+    public abstract void testGetExistResources() throws Exception;
+
+    /**
+     * 获取一个资源
+     *
+     * @throws Exception 异常
+     */
+    public abstract void testGetResource() throws Exception;
+
+    /**
+     * 添加一个资源
+     *
+     * @throws Exception 异常
+     */
+    public abstract void testPostResource() throws Exception;
+
+    /**
+     * 更新一个资源
+     *
+     * @throws Exception 异常
+     */
+    public abstract void testPutResource() throws Exception;
+
+    /**
+     * 更新一个资源
+     *
+     * @throws Exception 异常
+     */
+    public abstract void testPatchResource() throws Exception;
+
+    /**
+     * 删除资源
+     *
+     * @throws Exception 异常
+     */
+    public abstract void testDeleteExistResource() throws Exception;
+
+}
---
+++ lesson-cloud-core/src/test/java/cn/edu/gzmu/controller/SysLogControllerTest.java
@@ -0,0 +1,111 @@
+package cn.edu.gzmu.controller;
+
+import cn.edu.gzmu.MockMvcInit;
+import cn.edu.gzmu.model.entity.SysLog;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.Assert.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+/**
+ * @author echo
+ * @version 1.0
+ * @date 19-4-13 11:31
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@FixMethodOrder(MethodSorters.JVM)
+public class SysLogControllerTest extends MockMvcInit {
+
+    private SysLog sysLog;
+
+    @Test
+    @Override
+    public void testGetResources() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/sysLogs").contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._links").exists())
+                .andExpect(jsonPath("$._embedded").exists())
+                .andExpect(jsonPath("$.page").exists())
+                .andReturn();
+        assertEquals(mapper.readTree(mvcResult.getResponse().getContentAsString())
+                        .get("_embedded").get("sysLogs").size(),
+                2);
+    }
+
+    @Test
+    @Override
+    public void testGetExistResources() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/sysLogs/search/all").contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._links").exists())
+                .andExpect(jsonPath("$._embedded").exists())
+                .andExpect(jsonPath("$.page").exists())
+                .andReturn();
+        assertEquals(mapper.readTree(mvcResult.getResponse().getContentAsString())
+                        .get("_embedded").get("sysLogs").size(),
+                3);
+    }
+
+    @Test
+    @Override
+    public void testGetResource() throws Exception {
+        getOneResource();
+    }
+
+    @Test
+    @Override
+    public void testPostResource() throws Exception {
+        sysLog = getOneResource();
+        sysLog.setName("测试添加");
+        mockMvc.perform(post("/sysLogs").contentType(APPLICATION_JSON)
+                .content(mapper.writeValueAsString(sysLog)))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    @Override
+    public void testPutResource() throws Exception {
+        sysLog = getOneResource();
+        sysLog.setId(1L);
+        mockMvc.perform(put("/sysLogs/1")
+                .contentType(APPLICATION_JSON)
+                .content(mapper.writeValueAsString(sysLog)))
+                    .andExpect(status().isNoContent());
+    }
+
+    @Test
+    @Override
+    public void testPatchResource() throws Exception {
+        sysLog = getOneResource();
+        sysLog.setId(1L);
+        mockMvc.perform(patch("/sysLogs/1")
+                .contentType(APPLICATION_JSON)
+                .content(mapper.writeValueAsString(sysLog)))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    @Override
+    public void testDeleteExistResource() throws Exception {
+        mockMvc.perform(delete("/sysLogs/3"))
+                .andExpect(status().isNoContent());
+    }
+
+    private SysLog getOneResource() throws Exception {
+        String content = mockMvc.perform(get("/sysLogs/1").contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._links").exists())
+                .andExpect(jsonPath("$.name").value("测试日志1"))
+                .andReturn().getResponse().getContentAsString();
+        return mapper.readValue(content, SysLog.class);
+    }
+}
