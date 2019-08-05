3703e7fbc1fe40543e7c88acbfb5a5d1566d6dcc
YPXWYT <2568824264@qq.com>
2019-08-05 09:48:50 +0800

    :poop: 更新获取完整考试信息接口及获取完整考试规则接口

--- lesson-cloud-service/src/main/java/cn/edu/gzmu/service/impl/ExamServiceImpl.java
+++ lesson-cloud-service/src/main/java/cn/edu/gzmu/service/impl/ExamServiceImpl.java
@@ -1,10 +1,16 @@
 package cn.edu.gzmu.service.impl;
 
 import cn.edu.gzmu.model.entity.Exam;
+import cn.edu.gzmu.model.exception.ResourceNotFoundException;
+import cn.edu.gzmu.repository.entity.CourseRepository;
 import cn.edu.gzmu.repository.entity.ExamRepository;
+import cn.edu.gzmu.repository.entity.LogicClassRepository;
 import cn.edu.gzmu.service.ExamService;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 
 /**
@@ -18,5 +24,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ExamServiceImpl extends BaseServiceImpl<ExamRepository, Exam, Long>
         implements ExamService {
+    private final @NonNull ExamRepository examRepository;
+    private final @NonNull CourseRepository courseRepository;
 
+    @Override
+    protected Exam completeEntity(Exam entity) {
+        Exam exam ;
+        exam = examRepository.getOne(entity.getId());
+        exam.setCourse(courseRepository.getOne(entity.getCourseId()));
+        return exam;
+    }
 }
