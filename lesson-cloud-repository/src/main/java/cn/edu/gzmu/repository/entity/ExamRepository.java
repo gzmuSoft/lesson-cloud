eef1c64f011cd87ba1e1a4fb75bf52039a805c53
LJQ21 <2652214165@qq.com>
2019-08-09 18:34:14 +0800

    Merge branch 'dev' of /home/ljq/IdeaProjects/lesson-cloud with conflicts.

--- lesson-cloud-repository/src/main/java/cn/edu/gzmu/repository/entity/ExamRepository.java
+++ lesson-cloud-repository/src/main/java/cn/edu/gzmu/repository/entity/ExamRepository.java
@@ -4,7 +4,12 @@ import cn.edu.gzmu.model.entity.Exam;
 import cn.edu.gzmu.repository.base.BaseRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.data.rest.core.annotation.Description;
+
+import java.util.List;
 
 
 /**
@@ -16,6 +21,8 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  *
  * @author Japoul
  * @date 2019-8-06 22:38:13
+ *
+ * @author ljq
  */
 @RepositoryRestResource(path = "/exam")
 public interface ExamRepository extends BaseRepository<Exam, Long> {
@@ -30,4 +37,14 @@ public interface ExamRepository extends BaseRepository<Exam, Long> {
      */
     Page<Exam> findAllByCourseIdAndLogicClassIds(Long courseId, String classIds, Pageable pageable);
 
+    /**
+     * 根据逻辑班级分页查询考试信息
+     *
+     * @param ids 逻辑班级
+     * @param pageable 分页
+     * @return 考试信息
+     */
+    @RestResource(path = "exam", rel = "exam", description = @Description("通过学生所在逻辑班级查询所有资源"))
+    @Query(value = "SELECT * FROM exam WHERE logic_class_ids in :ids", nativeQuery = true)
+    Page<Exam> findAllByLogicClassIds(List<Long> ids, Pageable pageable);
 }
