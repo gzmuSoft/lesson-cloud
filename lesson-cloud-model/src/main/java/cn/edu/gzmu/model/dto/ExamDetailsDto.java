d50703971540d457849f00cefa4a1a29abb2c2c2
EchoCow <lizhongyue248@163.com>
2019-08-09 10:52:57 +0800

    :alien: Updating code due to external API changes for dto.

---
+++ lesson-cloud-model/src/main/java/cn/edu/gzmu/model/dto/ExamDetailsDto.java
@@ -0,0 +1,16 @@
+package cn.edu.gzmu.model.dto;
+
+import lombok.*;
+
+/**
+ * 考试详情 dto  请自行完善
+ *
+ * @author <a href="https://echocow.cn">EchoCow</a>
+ * @date 2019/8/9 上午10:51
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class ExamDetailsDto {
+}
